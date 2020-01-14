0c106d6bf6ce0641d614cb27043dddcf76cdd708
manupy <manuthambi@gmail.com>
2020-01-14 22:27:38 +0530

    second commit

--- lanuagebasics/first_progm.py
+++ lanuagebasics/first_progm.py
@@ -1,1 +1,10 @@
-print("hello ")
+#print("hello ")
+a=105
+b=20
+c=a+b
+print ("addition = ",c)
+print ("multiplication ",a*b)
+print ("division ",a/b)
+print ("mod division ",a%b)
+print ("expo ",a**b)
+print ("floor division ",a//b)
