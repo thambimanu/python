6efdbeb7f51032cdf8e64ee881595f53ef5a3a41
thambimanu <59815752+thambimanu@users.noreply.github.com>
2022-11-19 14:14:44 +0530

    Create loops.py

---
+++ loops.py
@@ -0,0 +1,17 @@
+#print values till n-1
+for i in range(n):
+  print(n)
+# from a to b-1
+for i in range(a,b):
+  print(i)
+
+# to generate 0,5,10,..................100
+
+for i in range(0,101,5):
+  print(i)
+ 
+  
+# will print all values including  100 
+while(n<=100):
+  print(n)
+  n=n+1
